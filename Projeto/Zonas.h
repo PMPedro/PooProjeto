8668205807ff72bd8300cd75e4189c8c5a3a4360
Pedro Martins <61905272+PMPedro@users.noreply.github.com>
2023-11-03 16:16:27 +0000

    -BibliotecaStor

---
+++ Projeto/Zonas.h
@@ -0,0 +1,14 @@
+//
+// Created by pmanu on 28/10/2023.
+//
+
+#ifndef TP_ZONAS_H
+#define TP_ZONAS_H
+#include "Propriedades.h"
+
+class Zonas {
+Propriedades propriedades;
+};
+
+
+#endif //TP_ZONAS_H
--- Projeto/main.cpp
+++ Projeto/main.cpp
@@ -1,10 +1,15 @@
 #include <iostream>
+#include <string>
+#include <sstream>
+#include <iomanip>
+#include "Terminal.h"
 #include "Habitacao.h"
 
 using namespace std;
 
+
 int main() {
 
-    std::cout << "Hello, World!" << std::endl;
+cout<<"Hello World \n"  ;
     return 0;
 }
