95943c0405c0d3df5f726a76c2c4f8db75f24785
DvGriffzz <diogovieira3@hotmail.com>
2023-12-21 21:55:09 +0000

    validation check

--- PooProjeto/main.cpp
+++ PooProjeto/main.cpp
@@ -2,8 +2,6 @@
 #include <string>
 #include "Terminal.h"
 #include "Habitacao.h"
-#include "Validacao.h"
-#include "Executa.h"
 #include "Interface.h"
 
 using namespace term;
@@ -12,40 +10,32 @@ int main() {
 
     Terminal &t = Terminal::instance();
     string comando;
-
-
+    Habitacao habitacao;
+    Interface interface;
     //Código Interface
-    //std::string str_in;
+    //texto
     t << move_to(0,0) << "[Trabalho de POO]";
-    //str_in = "";
-    //t >> str_in;
-
     t << move_to(0, 3) << "[Habitacao]:";
-
     t << move_to(0, 34 ) << "[Terminal]";
-    Window getData = Window(0, 35, t.getNumCols() , 5);
     t << move_to(104, 0) << "[Lista de Comandos]:";
+
+    //cria as janelas de output de texto e o terminal de input do dono
+    Window getData = Window(0, 35, t.getNumCols() , 5);
     Window listComandos = Window(t.getNumCols()/1.5, 1, t.getNumCols()/3,t.getNumRows() / 2);
 
     //Tratamento de Comandos
     std::vector<std::string> check;
     int conta = 1;
+
     while( comando != "sair" ) {
         getData >> comando;
         check.clear();
-        check = trataComando(comando);
+        check = interface.trataComando(comando);
+
         auto it = check.begin();
 
-        if (check.size() > 1){
-            listComandos <<"\nFicheiro:\n\n";
-            for(;it != check.end();++it){
-                listComandos << "\t" << *it;
-            }
-            getData.clear();
-        }
         if(check[0] == "true") {
             listComandos << "Comando '" << comando << "' validado.\n";
-            //execHnova(comando);
             ++conta;
             getData.clear();
         }else if(check[0] == "false"){
@@ -54,6 +44,14 @@ int main() {
             getData.clear();
         }
 
+        //caso do exec
+        if (check.size() > 1){
+            listComandos <<"\nFicheiro:\n\n";
+            for(;it != check.end();++it){
+                listComandos << "\t" << *it;
+            }
+            getData.clear();
+        }
         if (conta == 19){
             listComandos.clear();
             conta = 1;
